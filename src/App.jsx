46631e48cee20192df58144b1d01cfede41c9fa0
GermanGambarte <germangambarte@gmail.com>
2022-11-29 16:46:38 -0300

    add: layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Modal } from './components/Modal.component'
+import { Dashboard } from './components/Dashboard.component'
+import { List } from './components/List.component'
 import './App.css'
 
 const App = () => {
-  return <p>Holuu</p>
+  return (
+    <main>
+      <h1>Gasto Semanal</h1>
+      <div className="btn-container">
+        <button>Definir presupuesto</button>
+        <button>Agregar gasto</button>
+      </div>
+      <Modal />
+      <div className="container">
+        <Dashboard />
+        <List />
+      </div>
+    </main>
+  )
 }
 
 export default App
