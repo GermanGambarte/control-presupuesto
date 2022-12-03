37cdcbdb12b7e12b8769452303b79a4082d16faf
GermanGambarte <germangambarte@gmail.com>
2022-12-02 22:16:22 -0300

    add: Dashboard Content

--- src/components/Dashboard.component.jsx
+++ src/components/Dashboard.component.jsx
@@ -1,3 +1,23 @@
+import {
+  Box,
+  CircularProgress,
+  CircularProgressLabel,
+  Text,
+} from '@chakra-ui/react'
+
 export const Dashboard = () => {
-  return <p>Dashboard</p>
+  return (
+    <Box m="0 auto" w="80%">
+      <CircularProgress size="300px" value={40}>
+        <CircularProgressLabel fontSize="40px">
+          40% <Text>Gastado</Text>
+        </CircularProgressLabel>
+      </CircularProgress>
+      <Box>
+        <Text>Presupuesto: $00,00</Text>
+        <Text>Disponible: $00,00</Text>
+        <Text>Gastado: $00,00</Text>
+      </Box>
+    </Box>
+  )
 }
